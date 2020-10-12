b7b53ba8f771f4732af49c24eeb3bb0dace49ef2
Cheryl <cheryl.nqj@gmail.com>
2020-10-13 01:29:34 +0800

    cheryl/add-hello-list

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './HelloWorldList.css';
 import HelloWorld from './HelloWorld';
+import AddGreeter from './AddGreeter';
 
 class HelloWorldList extends Component {
     constructor(props) {
         super(props);
         this.state = { greetings: ['Jim', 'Sally', 'Bob'] };
+        this.addGreeting = this.addGreeting.bind(this);
     }
 
     renderGreetings() {
@@ -14,9 +16,14 @@ class HelloWorldList extends Component {
         });
     }
 
+    addGreeting(newName) {
+        this.setState({ greetings: [...this.state.greetings, newName] })
+    }
+
     render() {
         return (
             <div className="HelloWorldList">
+                <AddGreeter addGreeting={this.addGreeting} />
                 {this.renderGreetings()}
             </div>
         );
