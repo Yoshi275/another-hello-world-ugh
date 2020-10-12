a1ee58e6d679ec114f8e0edbb0d0d058f97d91ee
Cheryl <cheryl.nqj@gmail.com>
2020-10-13 00:29:58 +0800

    Talk to a robot too!

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -4,20 +4,37 @@ import './HelloWorld.css'
 class HelloWorld extends Component {
   constructor(props) {
     super(props);
-    this.state = { greeting: 'Hello' }
+    this.state = { greeting: 'Hello', isEnglish: true }
     this.frenchify = this.frenchify.bind(this);
+    this.anglicise = this.anglicise.bind(this);
+    this.beepBoop = this.beepBoop.bind(this);
   }
 
   frenchify() {
-    this.setState({ greeting: "Bonjour" });
+    this.setState({ greeting: "Bonjour", isEnglish: false });
+  }
+
+  anglicise() {
+    this.setState({ greeting: "Hello", isEnglish: true });
+  }
+
+  beepBoop() {
+    this.setState({ greeting: "01001000 01100101 01101100 01101100 01101111 " });
   }
 
   render() {
     return (
       <div className="HelloWorld">
-        <p>{this.state.greeting}, {this.props.name}!</p>
+        <p 
+          title="Secret button-thing! Press to communicate with a robot!"
+          onClick={this.beepBoop}>
+          {this.state.greeting}, {this.props.name}!
+        </p>
         <br />
-        <button onClick={this.frenchify}>Frenchify!</button>
+        { this.state.isEnglish
+          ? <button onClick={this.frenchify}>Frenchify!</button>
+          : <button onClick={this.anglicise}>Conquer!</button>
+        }
       </div>
     );
   }
